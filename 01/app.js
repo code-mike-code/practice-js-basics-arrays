1d8b3772f192682683d279e812091a7274688e6c
Michał <m.majewski.dm@gmail.com>
2024-06-09 22:57:19 +0200

    task-02-done

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+const users = ['Jan Kowalski', 'Kasia Kowalska', 'Damian Kowalski', 'Ewa Nowak', 'Jan Nowak']
+
+console.log('Użytkownik nr 1:', users[0])
+console.log('Użytkownik nr 3:', users[2])
+console.log('Użytkownik nr 5:', users[4])
+console.log('Użytkownik nr 1:', users.length)
--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,22 @@
-const randomArray = createRandomArray();
-console.log(randomArray);
+const randomArray = createRandomArray()
+console.log(randomArray)
 
+// petla FOR
+console.log('Elementy tablicy wyswietlone prz użyciu petli FOR:')
+for (let i = 0; i < randomArray.length; i++) {
+	console.log(randomArray[i])
+}
 
+//metoda FOR EACH
+
+console.log('Elementy tablicy wyswietlone prz użyciu metody forEach:')
+randomArray.forEach(element => {
+	console.log(element)
+})
+
+// ostatni element tblicy
+const lastElement = randomArray[randomArray.length - 1]
+console.log('Ostatni element tablicy(array):', lastElement)
 
 // nie modyfikuj kodu poniżej!
 
@@ -9,15 +24,15 @@ console.log(randomArray);
 // ponieważ w JS występuje mechanizm tzw. hoisting-u
 
 function createRandomArray() {
-    const arr = [];
-    const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
-    }
+	const arr = []
+	const len = getRandomInteger(1, 10)
+	for (let i = 0; i < len; i++) {
+		arr.push(getRandomInteger(1, 100))
+	}
 
-    return arr;
+	return arr
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+	return Math.round(Math.random() * (max - min) + min)
 }
